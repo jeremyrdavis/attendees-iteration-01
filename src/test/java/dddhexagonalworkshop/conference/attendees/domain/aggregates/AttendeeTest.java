6a0fdf96fd93fd117fa3d0f592caaf1cba19704a
jeremyrdavis <jeremyrdavis@mac.com>
2025-05-27 19:48:21 -0400

    test: add initial AttendeeTest class structure

---
+++ src/test/java/dddhexagonalworkshop/conference/attendees/domain/aggregates/AttendeeTest.java
@@ -0,0 +1,8 @@
+package dddhexagonalworkshop.conference.attendees.domain.aggregates;
+
+import dddhexagonalworkshop.conference.attendees.domain.valueobjects.Address;
+import org.junit.jupiter.api.Test;
+
+public class AttendeeTest {
+
+}
